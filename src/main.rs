749fc926ad23cf45824d96083323af9ec16eafc0
James Whaley <juicy66173@gmail.com>
2018-07-06 12:59:32 +0100

    Add run function and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 extern crate clap;
+#[macro_use]
+extern crate pkg;
 
 mod error;
 
-fn main() {}
+use std::process;
+
+use error::{Error, Result};
+
+fn run() -> Result<bool> {
+    Ok(true)
+}
+
+fn main() {
+    let result = run();
+
+    match result {
+        Err(error) => {
+            eprintln!("error: {}", error);
+            process::exit(1);
+        }
+        Ok(false) => process::exit(1),
+        Ok(true) => process::exit(0),
+    }
+}
